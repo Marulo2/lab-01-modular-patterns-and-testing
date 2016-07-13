38e39d618b1d25d0d1ee9c2a05534513491df5d9
Mars <marshallmiller92@gmail.com>
2016-07-13 11:48:15 -0700

    Gulp is watching

--- lab-Marshall/lib/gulpfile.js
+++ lab-Marshall/lib/gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const watch = require('gulp-watch');
 
-var testFiles = ['test/**/*.js'];
-var appFiles = ['lib/**/*.js'];
+var testFiles = ['test/*.js'];
+var appFiles = ['lib/*.js'];
+
+gulp.task('default', ['lint:app', 'lint:test', 'test', 'watch' ], () => {
+  console.log('Gulp is running');
+});
 
 gulp.task('lint:app', () => {
   gulp.src(appFiles)
@@ -19,4 +25,18 @@ gulp.task('lint:app', () => {
     .pipe(eslint.format());
 });
 
-gulp.task('default', ['lint:app', 'lint:test']);
+gulp.task('lint:test', () => {
+  gulp.src(testFiles)
+    .pipe(eslint());
+});
+
+
+gulp.task('test', () => {
+  gulp.src(testFiles)
+    .pipe(mocha());
+});
+
+gulp.task('watch', () => {
+  console.log('Watch is running');
+  gulp.watch('./**/*.js', ['lint:app', 'lint:test', 'test']);
+});
