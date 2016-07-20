aba80f5a6d740de40b946ed933060988217d6d41
Mars <marshallmiller92@gmail.com>
2016-07-20 11:46:44 -0700

    improved idiomatic style

--- lab-Marshall/lib/cowsay.js
+++ lab-Marshall/lib/cowsay.js
@@ -2,8 +2,8 @@
 
 const moo = require('cowsay');
 
-const cowsay = module.exports = function(a) {
-  let newText = process.argv[2] || a;
+const cowsay = module.exports = function(defaultMoo) {
+  let newText = process.argv[2] || defaultMoo;
   return moo.say({
     text : newText,
     e : '> <'
